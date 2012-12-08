c6fa264c5c6e54bf74e8cdbeebc511727ff5783c
Darrell Sandstrom <darrell.sandstrom@gmail.com>
2012-12-07 18:04:58 -0800

    Add separate sub cat data file

--- _antoniniknives_init.js
+++ _antoniniknives_init.js
@@ -36,7 +36,7 @@ app.rq.push(['script',0,app.vars.baseURL+'model.js']); //'validator':function(){
 app.rq.push(['script',0,app.vars.baseURL+'includes.js']); //','validator':function(){return (typeof handlePogs == 'function') ? true : false;}})
 app.rq.push(['script',1,app.vars.baseURL+'jeditable.js']); //used for making text editable (customer address). non-essential. loaded late.
 app.rq.push(['script',0,app.vars.baseURL+'controller.js']);
-// app.rq.push(['script',0,app.vars.baseURL+'_antoniniknives_subcats.json']);
+app.rq.push(['script',0,app.vars.baseURL+'_antoniniknives_subcatData.js']);
 // app.rq.push(['script',0,app.vars.baseURL+'jcarousel/carousel-ad.js']);
 
 //cycle used for slideshow
@@ -127,142 +127,142 @@ var subcatData = [];
 //   });
 // });
 
-// sub category data
-var subcatData = {
-  // category:
-  //   subcategory:
-  //     fields:
-  ".boat_-_fishing": {
-    ".antonini": {
-      "prettyLong": "Antonini Long Pretty", // missing
-      "description": "" // missing
-    },
-    ".boat" : {
-      "prettyLong":"Boat Multipurpose Yachting Knives",
-      "description" : "" // missing
-    },
-    ".classic" : {
-      "prettyLong":"Classic Fisherman Knives",
-      "description" : "The Classic Line of knives are great basic Fisherman's Knives covering a range of tasks. The blades are in 2 sizes (190 & 160 mm) made of quality stainless steel. Plastic handles."
-    },
-    ".fontanin" : {
-      "prettyLong":"Fontanin Fisherman Knives",
-      "description" : "In 2003 Antonini acquired the business of Messrs.Fontanin to continue crafting one of the most popular Italian fishing knives already sold worldwide in millions of pieces. Featuring ABS plastic handles (now available in 7 different colors: red, black, orange, green, yellow, blue and white and with convenient lanyard hole - some colors by special order only) and Aisi 420 stainless steel blades. <br/><br/>The plastic handle of this knife is particularly suited to effective customizing with your company logo (heat printing, screen printing and pad printing methods all available)."
-    },
-    ".nauta" : {
-      "prettyLong":"Nauta Boating and Pro-Rescue Knives",
-      "description" : "" // missing
-    }
-  },
-  ".cable_-_electrical": {
-    ".ampere": {
-      "prettyLong": "Ampere Professional Electrician Knives",
-      "description": "" // missing
-    },
-    ".export": {
-      "prettyLong": "Export Series Professional Electrician Knives",
-      "description": "" // missing
-    },
-    ".ohm": {
-      "prettyLong": "Ohm Professional Electrician Knives Insulated",
-      "description": "" // missing
-    },
-    ".volt": {
-      "prettyLong": "Volt Professional Electrician Knives",
-      "description": "" // missing
-    },
-    ".watt": {
-      "prettyLong": "Watt Professional Electrician Knives",
-      "description": "" // missing
-    }
-  },
-  ".farm_-_garden": {
-    ".classic": {
-      "prettyLong": "Classical Full-Arch Billhook Pruning Knives ",
-      "description": "" // missing
-    },
-    ".high-tech": {
-      "prettyLong": "High-Tech Pruning and Grafting Knives",
-      "description": "" // missing
-    },
-    ".hobby": {
-      "prettyLong": "Hobby Pruning and Grafting Knives",
-      "description": "" // missing
-    },
-    ".professional": {
-      "prettyLong": "Professional Pruning and Grafting Knives",
-      "description": "" // missing
-    },
-    ".traditional": {
-      "prettyLong": "Traditional Pruning Knives",
-      "description": "" // missing
-    }
-  },
-  ".pocket_-_traditional": {
-    ".10bullet": {
-      "prettyLong": "Bullet Pocket Knives",
-      "description": "" // missing
-    },
-    ".20cartridge": {
-      "prettyLong": "12-Guage Shotgun Cartridge Knives",
-      "description": "" // missing
-    },
-    ".30maniaghese": {
-      "prettyLong": "Traditional Pocket Knives of Maniago",
-      "description": "" // missing
-    },
-    ".40pattada": {
-      "prettyLong": "Traditional Pocket Knives of Pattada",
-      "description": "" // missing
-    },
-    ".50siciliano": {
-      "prettyLong": "Traditional Pocket Knives of Sicily",
-      "description": "" // missing
-    },
-    ".60other-pocket": {
-      "prettyLong": "Other Pocket Knives",
-      "description": "" // missing
-    }
-  },
-  ".sos_-_rescue": {
-    ".10ara-xl": {
-      "prettyLong": "ARA XL Professional Rescue Knives",
-      "description": "" // missing
-    },
-    ".20nauta": {
-      "prettyLong": "Nauta Boating and Pro-Rescue Knives",
-      "description": "" // missing
-    },
-    ".30mamba": {
-      "prettyLong": "Mamba Pro-Rescue Knives with Fixed Blades",
-      "description": "" // missing
-    },
-    ".40narmada": {
-      "prettyLong": "Narmada Tactical Pro-Rescue Knives",
-      "description": "" // missing
-    },
-    ".50si-plus": {
-      "prettyLong": "SI Plus Pro-Rescue Knives",
-      "description": "" // missing
-    },
-    ".60uri-xl": {
-      "prettyLong": "URI XL Pro-Rescue Knives",
-      "description": "" // missing
-    },
-    ".70eighty-plus": {
-      "prettyLong": "80 Plus Pro-Rescue Knives Anniversary Edition",
-      "description": "To celebrate their 80th Anniversary in 2005 (1925-2005), A. E. Coltellerie (Antonini Knives) created this very special series of the SOS rescue knives. Due to their popularity, production continues on these very useful, very well-crafted knives."
-    },
-    ".90other-sos": {
-      "prettyLong": "Other SOS Professional Rescue Tools",
-      "description": "" // missing
-    },
-    ".95sos-sheaths": {
-      "prettyLong": "Sheaths for Various SOS Products",
-      "description": "" // missing
-    }
-  }
-};
+// // sub category data
+// var subcatData = {
+//   // category:
+//   //   subcategory:
+//   //     fields:
+//   ".boat_-_fishing": {
+//     ".antonini": {
+//       "prettyLong": "Antonini Long Pretty", // missing
+//       "description": "" // missing
+//     },
+//     ".boat" : {
+//       "prettyLong":"Boat Multipurpose Yachting Knives",
+//       "description" : "" // missing
+//     },
+//     ".classic" : {
+//       "prettyLong":"Classic Fisherman Knives",
+//       "description" : "The Classic Line of knives are great basic Fisherman's Knives covering a range of tasks. The blades are in 2 sizes (190 & 160 mm) made of quality stainless steel. Plastic handles."
+//     },
+//     ".fontanin" : {
+//       "prettyLong":"Fontanin Fisherman Knives",
+//       "description" : "In 2003 Antonini acquired the business of Messrs.Fontanin to continue crafting one of the most popular Italian fishing knives already sold worldwide in millions of pieces. Featuring ABS plastic handles (now available in 7 different colors: red, black, orange, green, yellow, blue and white and with convenient lanyard hole - some colors by special order only) and Aisi 420 stainless steel blades. <br/><br/>The plastic handle of this knife is particularly suited to effective customizing with your company logo (heat printing, screen printing and pad printing methods all available)."
+//     },
+//     ".nauta" : {
+//       "prettyLong":"Nauta Boating and Pro-Rescue Knives",
+//       "description" : "" // missing
+//     }
+//   },
+//   ".cable_-_electrical": {
+//     ".ampere": {
+//       "prettyLong": "Ampere Professional Electrician Knives",
+//       "description": "" // missing
+//     },
+//     ".export": {
+//       "prettyLong": "Export Series Professional Electrician Knives",
+//       "description": "" // missing
+//     },
+//     ".ohm": {
+//       "prettyLong": "Ohm Professional Electrician Knives Insulated",
+//       "description": "" // missing
+//     },
+//     ".volt": {
+//       "prettyLong": "Volt Professional Electrician Knives",
+//       "description": "" // missing
+//     },
+//     ".watt": {
+//       "prettyLong": "Watt Professional Electrician Knives",
+//       "description": "" // missing
+//     }
+//   },
+//   ".farm_-_garden": {
+//     ".classic": {
+//       "prettyLong": "Classical Full-Arch Billhook Pruning Knives ",
+//       "description": "" // missing
+//     },
+//     ".high-tech": {
+//       "prettyLong": "High-Tech Pruning and Grafting Knives",
+//       "description": "" // missing
+//     },
+//     ".hobby": {
+//       "prettyLong": "Hobby Pruning and Grafting Knives",
+//       "description": "" // missing
+//     },
+//     ".professional": {
+//       "prettyLong": "Professional Pruning and Grafting Knives",
+//       "description": "" // missing
+//     },
+//     ".traditional": {
+//       "prettyLong": "Traditional Pruning Knives",
+//       "description": "" // missing
+//     }
+//   },
+//   ".pocket_-_traditional": {
+//     ".10bullet": {
+//       "prettyLong": "Bullet Pocket Knives",
+//       "description": "" // missing
+//     },
+//     ".20cartridge": {
+//       "prettyLong": "12-Guage Shotgun Cartridge Knives",
+//       "description": "" // missing
+//     },
+//     ".30maniaghese": {
+//       "prettyLong": "Traditional Pocket Knives of Maniago",
+//       "description": "" // missing
+//     },
+//     ".40pattada": {
+//       "prettyLong": "Traditional Pocket Knives of Pattada",
+//       "description": "" // missing
+//     },
+//     ".50siciliano": {
+//       "prettyLong": "Traditional Pocket Knives of Sicily",
+//       "description": "" // missing
+//     },
+//     ".60other-pocket": {
+//       "prettyLong": "Other Pocket Knives",
+//       "description": "" // missing
+//     }
+//   },
+//   ".sos_-_rescue": {
+//     ".10ara-xl": {
+//       "prettyLong": "ARA XL Professional Rescue Knives",
+//       "description": "" // missing
+//     },
+//     ".20nauta": {
+//       "prettyLong": "Nauta Boating and Pro-Rescue Knives",
+//       "description": "" // missing
+//     },
+//     ".30mamba": {
+//       "prettyLong": "Mamba Pro-Rescue Knives with Fixed Blades",
+//       "description": "" // missing
+//     },
+//     ".40narmada": {
+//       "prettyLong": "Narmada Tactical Pro-Rescue Knives",
+//       "description": "" // missing
+//     },
+//     ".50si-plus": {
+//       "prettyLong": "SI Plus Pro-Rescue Knives",
+//       "description": "" // missing
+//     },
+//     ".60uri-xl": {
+//       "prettyLong": "URI XL Pro-Rescue Knives",
+//       "description": "" // missing
+//     },
+//     ".70eighty-plus": {
+//       "prettyLong": "80 Plus Pro-Rescue Knives Anniversary Edition",
+//       "description": "To celebrate their 80th Anniversary in 2005 (1925-2005), A. E. Coltellerie (Antonini Knives) created this very special series of the SOS rescue knives. Due to their popularity, production continues on these very useful, very well-crafted knives."
+//     },
+//     ".90other-sos": {
+//       "prettyLong": "Other SOS Professional Rescue Tools",
+//       "description": "" // missing
+//     },
+//     ".95sos-sheaths": {
+//       "prettyLong": "Sheaths for Various SOS Products",
+//       "description": "" // missing
+//     }
+//   }
+// };
 
 
 /// functions \\\
