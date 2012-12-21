003decaca92038bd7864b90541ae1b7cc6f9848c
gourmetseedint <sales@gourmetseed.com>
2012-12-21 11:43:37 -0700

    Update _antoniniknives_catData.js

--- _antoniniknives_catData.js
+++ _antoniniknives_catData.js
@@ -22,15 +22,15 @@ var catData = {
   ],
   ".about_gsi_distributing": [
     {
-      "image": "about_image_1.png",
+      "image": "GSIDistLogo.png",
       "paragraph": "GSI Distributing is a d/b/a of Gourmet Seed International, LLC and is the official North American distributor for A. E. Coltellerie snl, the makers of Antonini Knives."
     },
     {
-      "image": "GSIDistLogo.png",
+      "image": "ISATlogo.png",
       "paragraph": "Formed in 2005 as a merger of several family-owned companies (Good Earth Seed, Harvest Moon Farms, and Italian Seed and Tool), Gourmet Seed International, LLC is still family owned as a joint venture of the Harris and Winteregg families. Located in rural Southeast New Mexico, we bring old-fashioned, small-town values into everything we do."
     },
     {
-      "image":"about_image_3.png",
+      "image":"GSIdisplaylogo.png",
       "paragraph": "GSI Distributing is focused on bringing fine and unusual products to the North American market. Using the efficiencies of e-commerce, we are proud to bring these wonderful products to you with great service and at prices that represent a good value."
     },
     {
@@ -42,19 +42,19 @@ var catData = {
   ],
   ".about-antonini": [
     {
-      "image": "",
+      "image": "cutting-blades.png",
       "paragraph": "Founded in 1925, A. E. Coltellerie snl has been creating great knives under the \"Antonini\" brand for more than three generations. The heritage of their location in the Maniago area near Pordenone, in northeast Italy, an area with a history of making knives, blades, and other cutting tools since Medieval times provides centuries of experience and skill."
     },
     {
-      "image": "",
+      "image": "Grinding.png",
       "paragraph": "Their present range of products are \"working\" tools, which consists mostly of folding pocket knives for agriculture, boating/fishing, and electrical/telephone/cable industrial workers and home hobbyists. Also in their range are traditional pocket knives of Italy. They are also the original designers and producers of the \"bullet\" and \"cartridge\" knives, combining novelty with quality and usefulness."
     },
     {
-      "image": "",
+      "image": "Sharpening.png",
       "paragraph": "Their most recent expansion has been in a range of knives specifically designed for use in emergency and rescue operations, which have been very warmly received by Police, Fire, and rescue crews worldwide. "
     },
     {
-      "image": "",
+      "image": "finishing-department.png",
       "paragraph": "You can be assured that the legendary \"Antonini\" label is one of quality associated with a family-owned company that still makes tools at their factory in Maniago, Italy. A. E. Coltellerie snl combines centuries of tradition and experience in metallurgy and knife-making with cutting edge technology and design, and they guarantee the quality of the materials and workmanship that goes into every knife they make."
     }
   ]
