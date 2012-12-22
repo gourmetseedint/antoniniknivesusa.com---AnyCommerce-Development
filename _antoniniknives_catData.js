aa8dbc882f38c3f19152b2c2b139fbac7a0ab54f
gourmetseedint <sales@gourmetseed.com>
2012-12-22 07:56:47 -0700

    Update _antoniniknives_catData.js

--- _antoniniknives_catData.js
+++ _antoniniknives_catData.js
@@ -8,29 +8,29 @@ var catData = {
   "imageDirectory": "category_images/",
   ".promo_-_customizing": [
     {
-      "image": "imprinting.png",
+      "image": "imprinting.jpg",
       "paragraph": "All knives with plastic or wooden handles can be customized with logos and company details.  The methods that can be used are thermal printing (for fairly simple logos or text), tampography or serigraphy with single or mulitple colors.   Proof of ownership or user authorization must be provided for printing logos or brand names. The logo or brand name to be printed must be provided on file, preferably in digital EPS or TIFF format. Click on WHOLESALE AVAILABLE to the left for estimates and minimum batch sizes for customizing."
     },
     {
-      "image": "laser.png",
+      "image": "laser.jpg",
       "paragraph": "Knives with anodized aluminum handles are particularly well suited to laser incision customizing. Laser incision removes the anodized color layer revealing the uniform single light grey shade of the original aluminum. Laser incision can also be performed on the blade and offers an excellent sense of contrast. This type of incision is permanent, cannot be modified and adds a degree of exclusivity to a product. For the reproduction of brand names and logos, proof of ownership or authorization must be provided and the logo or brand name needs to be provided in digital EPS or TIFF format.  Click on WHOLESALE AVAILABLE to the left for estimates and minimum batch sizes for customizing."
     },
     {
-      "image": "colors.png",
+      "image": "colors.jpg",
       "paragraph": "There are additional custom options available for many of the Antonini knives, including handle color, handle material (ex. wood, plastic, etc.), and blade type (ex. stainless or carbon).  Click on WHOLESALE AVAILABLE to the left for estimates and minimum batch sizes for customizing."
     }
   ],
   ".about_gsi_distributing": [
     {
-      "image": "GSIDistLogo.png",
+      "image": "GSIDistLogo.jpg",
       "paragraph": "GSI Distributing is a d/b/a of Gourmet Seed International, LLC and is the official North American distributor for A. E. Coltellerie snl, the makers of Antonini Knives."
     },
     {
-      "image": "GSIdisplaylogo.png",
+      "image": "GSIdisplaylogo.jpg",
       "paragraph": "Formed in 2005 as a merger of several family-owned companies (Good Earth Seed, Harvest Moon Farms, and Italian Seed and Tool), Gourmet Seed International, LLC is still family owned as a joint venture of the Harris and Winteregg families. Located in rural Southeast New Mexico, we bring old-fashioned, small-town values into everything we do."
     },
     {
-      "image":"ISATlogo.png",
+      "image":"ISATlogo.jpg",
       "paragraph": "GSI Distributing is focused on bringing fine and unusual products to the North American market. Using the efficiencies of e-commerce, we are proud to bring these wonderful products to you with great service and at prices that represent a good value."
     },
     {
@@ -42,19 +42,19 @@ var catData = {
   ],
   ".about-antonini": [
     {
-      "image": "1 - Pressing the blade.jpg",
+      "image": "cutting.jpg",
       "paragraph": "Founded in 1925, A. E. Coltellerie snl has been creating great knives under the \"Antonini\" brand for more than three generations. The heritage of their location in the Maniago area near Pordenone, in northeast Italy, an area with a history of making knives, blades, and other cutting tools since Medieval times provides centuries of experience and skill."
     },
     {
-      "image": "Grinding.png",
+      "image": "grinding.jpg",
       "paragraph": "Their present range of products are \"working\" tools, which consists mostly of folding pocket knives for agriculture, boating/fishing, and electrical/telephone/cable industrial workers and home hobbyists. Also in their range are traditional pocket knives of Italy. They are also the original designers and producers of the \"bullet\" and \"cartridge\" knives, combining novelty with quality and usefulness."
     },
     {
-      "image": "Sharpening.png",
+      "image": "sharpening.jpg",
       "paragraph": "Their most recent expansion has been in a range of knives specifically designed for use in emergency and rescue operations, which have been very warmly received by Police, Fire, and rescue crews worldwide. "
     },
     {
-      "image": "finishing-department.png",
+      "image": "finishing-department.jpg",
       "paragraph": "You can be assured that the legendary \"Antonini\" label is one of quality associated with a family-owned company that still makes tools at their factory in Maniago, Italy. A. E. Coltellerie snl combines centuries of tradition and experience in metallurgy and knife-making with cutting edge technology and design, and they guarantee the quality of the materials and workmanship that goes into every knife they make."
     }
   ]
