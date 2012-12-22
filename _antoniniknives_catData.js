836c4dad14f15393673204ae29c587abc8628ef3
gourmetseedint <sales@gourmetseed.com>
2012-12-22 08:10:35 -0700

    Update _antoniniknives_catData.js

--- _antoniniknives_catData.js
+++ _antoniniknives_catData.js
@@ -43,8 +43,7 @@ var catData = {
   ".about-antonini": [
     {
       "image": "cutting.jpg",
-      "paragraph": "Founded in 1925, A. E. Coltellerie snl has been creating great knives under the \"Antonini\" brand for more than three generations."
-      "paragraph": "The heritage of their location in the Maniago area near Pordenone, in northeast Italy provides centuries of experience and skill, as it is an area with a history of making knives, blades, and other cutting tools since Medieval times."
+      "paragraph": "Founded in 1925, A. E. Coltellerie snl has been creating great knives under the \"Antonini\" brand for more than three generations. The heritage of their location in the Maniago area near Pordenone in northeast Italy provides centuries of experience and skill, as it is an area with a history of making knives, blades, and other cutting tools since Medieval times."
     },
     {
       "image": "grinding.jpg",
